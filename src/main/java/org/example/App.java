aa7134f0d1e63b55f33f0956ef1fbf73476946f4
Alin2546 <eternalsq1337%gmail.com>
2024-11-12 01:30:23 +0200

    Test class

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,6 +1,5 @@
 package org.example;
 
-import java.util.*;
 
 /**
  * A simple class which contains a method for sorting an hashMap based on values.
@@ -13,42 +12,23 @@ import java.util.*;
  * @version 1.0
  */
 public class App {
-    /**
-     * @param map a HashMap to be sorted by value
-     * @return sorted map by values
-     */
-    public static HashMap<Integer, Integer> sortByValue(HashMap<Integer, Integer> map) {
-        List<Map.Entry<Integer, Integer>> list = new ArrayList<>(map.entrySet());
-        list.sort(new Comparator<Map.Entry<Integer, Integer>>() {
-            @Override
-            public int compare(Map.Entry<Integer, Integer> o1, Map.Entry<Integer, Integer> o2) {
-                return o1.getValue().compareTo(o2.getValue());
-            }
-        });
-        LinkedHashMap<Integer, Integer> sortedMap = new LinkedHashMap<>();
-        for (Map.Entry<Integer, Integer> entry : list) {
-            sortedMap.put(entry.getKey(), entry.getValue());
-        }
-        return sortedMap;
-    }
 
-    /**
-     * Entry point
-     *
-     * <p>
-     * Contains a predefined HashMap with random keys and values to test sortByValue method.
-     * </p>
-     *
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        map.put(2, 3);
-        map.put(5, 2);
-        map.put(4, 1);
-        map.put(123, 7);
-        map.put(121, 89);
-        map.put(6345, 7);
-        System.out.println(sortByValue(map));
+        LibraryCatalog<Book> catalog = new LibraryCatalog<>();
+        Book book1 = new Novel("Morometii", 150, "Fantasy");
+        Book book2 = new Novel("Morometii", 140, "Fantasy");
+        Book book3 = new Novel("Aln", 200, "Fantasy");
+        Book book4 = new ArtAlbum("Weeding", 25, "Glossy");
+
+
+        catalog.add(book1);
+        catalog.add(book2);
+        catalog.add(book3);
+        catalog.add(book4);
+        catalog.delete(book1);
+
+        catalog.list();
+
+
     }
 }
